b8855db516d1e90b4a63c4fad27ef67ec7fefa9a
Santiago Chivata <sac.94@hotmail.com>
2024-03-14 09:13:40 -0500

    added exercises of section 4

--- main.py
+++ main.py
@@ -51,3 +51,28 @@ num1 = "1"
 num2 = "2"
 print("Suma de variables string convertidas a int")
 print("suma: ", int(num1) + int(num2))
+print('\n')
+
+## Procesar entrada del usuario con la funcion input
+mensaje = input("Escribe un mensaje: ")
+print("El mensaje ingresado es: " + mensaje)
+print('\n')
+
+## Convertir tipo de datos junto con la funcion input ya que por defecto la funcion input devuelve un str
+numero1 = int(input("Ingresa el primer numero: "))
+numero2 = int(input("Ingresa el segundo numero: "))
+resultado = numero1 + numero2
+print("El resultado de la suma es: ", resultado)
+print('\n')
+
+## Ejercicio - Califica tu dia
+
+enunciado = int(input("Califica del 1 al 10 como estuvo tu dia: "))
+print("Tu dia estuvo de: ", enunciado)
+print('\n')
+
+## Ejercicio - Informacion de un libro
+libro = input("Ingresa el titulo del libro: ")
+autor = input("Ingresa el nomnre del autor del libro: ")
+print(libro ,"fue escrito por", autor)
+
